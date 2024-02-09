9e5d644491a09c72225783a87936d45c6de7f499
vineet2812 <vineet_pashine@persistent.com>
2024-02-09 13:30:08 +0530

    ngClass and ngStyle

--- Ekart/src/app/app.component.ts
+++ Ekart/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { TopHeaderComponent } from './components/top-header/top-header.component';
-import { ProductlistComponent } from './components/productlist/productlist.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,TopHeaderComponent,ProductlistComponent],
+  imports: [RouterOutlet,HeaderComponent,TopHeaderComponent,ContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Ekart/src/app/components/header/mainmenu/mainmenu.component.ts
+++ Ekart/src/app/components/header/mainmenu/mainmenu.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-mainmenu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mainmenu.component.html',
   styleUrl: './mainmenu.component.css'
 })
 export class MainmenuComponent {
-
+  listOfMenu: string[] = ['Home','Products', 'Sale', 'New Arrivals', 'Contacts']
 }
