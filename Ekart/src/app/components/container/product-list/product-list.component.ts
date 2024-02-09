1dd361b46c64d069756fb5d8e383a5501b5a5826
vineet2812 <vineet_pashine@persistent.com>
2024-02-09 13:59:07 +0530

    input decorator

--- Ekart/src/app/components/container/product-list/product-list.component.ts
+++ Ekart/src/app/components/container/product-list/product-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ProductComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
